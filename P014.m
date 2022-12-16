d0bc313f1cae035233f41de11351ff8919117703
elgar328 <cork_unsold_0k@icloud.com>
2022-12-16 14:34:00 +0900

    problem 14

---
+++ P014.m
@@ -0,0 +1,28 @@
+%% Project Euler - Problem 14
+% Longest Collatz sequence
+% https://projecteuler.net/problem=14
+
+sol = 1;
+len = 1;
+for n = 2:1000000
+    len_new = collatz_length(n);
+    if len < len_new
+        sol = n;
+        len = len_new;
+    end
+end
+disp(sol)
+
+function out = collatz_length(n)
+out = 1;
+while n~=1
+    out = out+1;
+    if mod(n,2) == 0
+        n = n/2;
+    else
+        n = n*3+1;
+    end
+end
+end
+
+% memoization 기법 사용해서 개선 요
